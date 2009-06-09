ed4bd6841fe3c9423e61044404337d8769cabb89
luke.maurits <luke.maurits@a65a53d6-54cd-11de-8ef9-cf35a3c3eb08>
2009-06-09 08:46:25 +0000

    Pulled a lot of stuff that should never have been inside the Feed class definition out of it
    git-svn-id: http://feedformatter.googlecode.com/svn/trunk@3 a65a53d6-54cd-11de-8ef9-cf35a3c3eb08

--- feedformatter.py
+++ feedformatter.py
@@ -50,6 +50,227 @@ except ImportError:
 
 from time import time, strftime, localtime, mktime, struct_time, timezone
 
+# RSS 1.0 Functions ----------
+
+_rss1_channel_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "link"),
+    (("description", "desc", "summary"), "description")
+)
+
+_rss1_item_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "link"),
+    (("description", "desc", "summary"), "description")
+)
+
+# RSS 2.0 Functions ----------
+
+_rss2_channel_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "link"),
+    (("description", "desc", "summary"), "description"),
+    (("pubDate", "pubdate", "date", "published", "updated"), "pubDate",
+        lambda(x): _format_datetime("rss2",x)),
+    (("category",), "category"),
+    (("language",), "language"),
+    (("copyright",), "copyright"),
+    (("webMaster",), "webmaster"),
+    (("image",), "image"),
+    (("skipHours",), "skipHours"),
+    (("skipDays",), "skipDays")
+)
+
+_rss2_item_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "link"),
+    (("description", "desc", "summary"), "description"),
+    (("guid", "id"), "guid"),
+    (("pubDate", "pubdate", "date", "published", "updated"), "pubDate",
+lambda(x): _format_datetime("rss2",x)),
+    (("category",), "category"),
+    (("author",), "author", lambda(x): _rssify_author(x))
+)
+
+# Atom 1.0 ----------
+
+_atom_feed_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "id"),
+    (("description", "desc", "summary"), "subtitle"),
+    (("pubDate", "pubdate", "date", "published", "updated"), "updated",
+lambda(x): _format_datetime("atom",x)),
+    (("category",), "category"),
+    (("author",), "author", lambda(x): _atomise_author(x))
+)
+
+_atom_item_mappings = (
+    (("title",), "title"),
+    (("link", "url"), "id"),
+    (("link", "url"), "link", lambda(x): _atomise_link(x)),
+    (("description", "desc", "summary"), "summary"),
+    (("pubDate", "pubdate", "date", "published", "updated"), "updated",
+lambda(x): _format_datetime("atom",x)),
+    (("category",), "category"),
+    (("author",), "author", lambda(x): _atomise_author(x))
+)
+
+def _get_tz_offset():
+
+    """
+    Return the current timezone's offset from GMT as a string
+    in the format +/-HH:MM, as required by RFC3339.
+    """
+
+    seconds = -1*timezone    # Python gets the offset backward! >:(
+    minutes = seconds/60
+    hours = minutes/60
+    minutes = minutes - hours*60
+    if seconds < 0:
+        return "%02d:%d" % (hours, minutes)
+    else:
+        return "+%02d:%d" % (hours, minutes)
+
+def _convert_datetime(time):
+
+    """
+    Convert time, which may be one of a whole lot of things, into a
+    standard 9 part time tuple.
+    """
+
+    if type(time) is tuple or type(time) is struct_time:
+        # Already done!
+        return time
+    elif type(time) is int or type(time) is float:
+        # Assume this is an epoch time
+        return localtime(time)
+    elif type(time) is str:    
+        if time.isalnum():
+            # String is alphanumeric - a time stamp?
+            try:
+                return strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
+            except ValueError:
+                raise Exception("Unrecongised time format!")        
+        else:
+            # Maybe this is a string of an epoch time?
+            try:
+                return localtime(float(time))
+            except ValueError:
+                # Guess not.
+                raise Exception("Unrecongised time format!")                 
+    else:
+        # No idea what this is.  Give up!
+        raise Exception("Unrecongised time format!")
+
+def _format_datetime(feed_type, time):
+
+    """
+    Convert a standard 9 part time tuple into a string which can be
+    used in a validly formatted feed of type feed_type.  Raise an
+    Exception if this cannot be done.
+    """
+    
+    # First, convert time into a time structure
+    time = _convert_datetime(time)
+
+    # Then, convert that to the appropriate string
+    if feed_type in ("rss1", "rss2"):
+        return strftime("%a, %d %b %Y %H:%M:%S %Z", time)
+    elif feed_type is "atom":
+        return strftime("%Y-%m-%dT%H:%M:%S", time) + _get_tz_offset();
+
+def _atomise_link(link):
+
+    if type(link) is dict:
+        return dict
+    else:
+        return {"href" : link}
+        
+def _atomise_author(author):
+
+    """
+    Convert author from whatever it is to a dictionary representing an
+    atom:Person construct.
+    """
+
+    if type(author) is dict:
+        return author
+    else:
+        if author.startswith("http://") or author.startswith("www"):
+            # This is clearly a URI
+            return {"uri" : author}
+        elif "@" in author and "." in author:
+            # This is most probably an email address
+            return {"email" : author}
+        else:
+            # Must be a name
+            return {"name" : author}
+
+def _rssify_author(author):
+
+    """
+    Convert author from whatever it is to a plain old email string for
+    use in an RSS 2.0 feed.
+    """
+    
+    if type(author) is dict:
+        try:
+            return author["email"]
+        except KeyError:
+            return None
+    else:
+        if "@" in author and "." in author:
+            return author
+        else:
+            return None
+
+def _add_subelems(root_element, mappings, dictionary):
+
+    """
+    Add one subelement to root_element for each key in dictionary
+    which is supported by a mapping in mappings
+    """
+
+    for mapping in mappings:
+        for key in mapping[0]:
+            if key in dictionary:
+                if len(mapping) == 2:
+                    value = dictionary[key]
+                elif len(mapping) == 3:
+                    value = mapping[2](dictionary[key])
+                _add_subelem(root_element, mapping[1], value)
+                break                
+    
+def _add_subelem(root_element, name, value):
+
+    if value is None:
+        return
+
+    if type(value) is dict:
+        ### HORRIBLE HACK!
+        if name=="link":
+            ET.SubElement(root_element, name, href=value["href"])
+        else:
+            subElem = ET.SubElement(root_element, name)
+            for key in value:
+                _add_subelem(subElem, key, value[key])
+    else:
+        ET.SubElement(root_element, name).text = value
+
+def dump_to_file(tree, filename, pretty):
+
+    """
+    Dump an ElementTree to a file, with line breaks and indentation.
+    """
+
+    fp = open(filename, "w")
+    if pretty and feedformatterCanPrettyPrint:
+        doc = FromXml(ET.tostring(tree))
+        PrettyPrint(doc,fp,indent="    ")
+    else:
+        fp.write(ET.tostring(tree))
+    fp.close()
+
 class Feed:
 
     ### INTERNAL METHODS ------------------------------
@@ -65,227 +286,6 @@ class Feed:
         else:
             self.items = []
         self.entries = self.items
-
-        # RSS 1.0 ----------
-
-        self.rss1_channel_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "link"),
-            (("description", "desc", "summary"), "description")
-        )
-
-        self.rss1_item_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "link"),
-            (("description", "desc", "summary"), "description")
-        )
-
-        # RSS 2.0 ----------
-
-        self.rss2_channel_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "link"),
-            (("description", "desc", "summary"), "description"),
-            (("pubDate", "pubdate", "date", "published", "updated"), "pubDate",
-                lambda(x): self._format_datetime("rss2",x)),
-            (("category",), "category"),
-            (("language",), "language"),
-            (("copyright",), "copyright"),
-            (("webMaster",), "webmaster"),
-            (("image",), "image"),
-            (("skipHours",), "skipHours"),
-            (("skipDays",), "skipDays")
-        )
-
-        self.rss2_item_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "link"),
-            (("description", "desc", "summary"), "description"),
-            (("guid", "id"), "guid"),
-            (("pubDate", "pubdate", "date", "published", "updated"), "pubDate",
-                lambda(x): self._format_datetime("rss2",x)),
-            (("category",), "category"),
-            (("author",), "author", lambda(x): self._rssify_author(x))
-        )
-
-        # Atom 1.0 ----------
-
-        self.atom_feed_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "id"),
-            (("description", "desc", "summary"), "subtitle"),
-            (("pubDate", "pubdate", "date", "published", "updated"), "updated",
-                lambda(x): self._format_datetime("atom",x)),
-            (("category",), "category"),
-            (("author",), "author", lambda(x): self._atomise_author(x))
-        )
-
-        self.atom_item_mappings = (
-            (("title",), "title"),
-            (("link", "url"), "id"),
-            (("link", "url"), "link", lambda(x): self._atomise_link(x)),
-            (("description", "desc", "summary"), "summary"),
-            (("pubDate", "pubdate", "date", "published", "updated"), "updated",
-                lambda(x): self._format_datetime("atom",x)),
-            (("category",), "category"),
-            (("author",), "author", lambda(x): self._atomise_author(x))
-        )
-
-    def _get_tz_offset(self):
-
-        """
-        Return the current timezone's offset from GMT as a string
-        in the format +/-HH:MM, as required by RFC3339.
-        """
-
-        seconds = -1*timezone    # Python gets the offset backward! >:(
-        minutes = seconds/60
-        hours = minutes/60
-        minutes = minutes - hours*60
-        if seconds < 0:
-            return "%02d:%d" % (hours, minutes)
-        else:
-            return "+%02d:%d" % (hours, minutes)
-
-    def _convert_datetime(self, time):
-
-        """
-        Convert time, which may be one of a whole lot of things, into a
-        standard 9 part time tuple.
-        """
-
-        if type(time) is tuple or type(time) is struct_time:
-            # Already done!
-            return time
-        elif type(time) is int or type(time) is float:
-            # Assume this is an epoch time
-            return localtime(time)
-        elif type(time) is str:    
-            if time.isalnum():
-                # String is alphanumeric - a time stamp?
-                try:
-                    return strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
-                except ValueError:
-                    raise Exception("Unrecongised time format!")        
-            else:
-                # Maybe this is a string of an epoch time?
-                try:
-                    return localtime(float(time))
-                except ValueError:
-                    # Guess not.
-                    raise Exception("Unrecongised time format!")                 
-        else:
-            # No idea what this is.  Give up!
-            raise Exception("Unrecongised time format!")
-
-    def _format_datetime(self, feed_type, time):
-
-        """
-        Convert a standard 9 part time tuple into a string which can be
-        used in a validly formatted feed of type feed_type.  Raise an
-        Exception if this cannot be done.
-        """
-        
-        # First, convert time into a time structure
-        time = self._convert_datetime(time)
-
-        # Then, convert that to the appropriate string
-        if feed_type in ("rss1", "rss2"):
-            return strftime("%a, %d %b %Y %H:%M:%S %Z", time)
-        elif feed_type is "atom":
-            return strftime("%Y-%m-%dT%H:%M:%S", time) + self._get_tz_offset();
-
-    def _atomise_link(self, link):
-
-        if type(link) is dict:
-            return dict
-        else:
-            return {"href" : link}
-            
-    def _atomise_author(self, author):
-
-        """
-        Convert author from whatever it is to a dictionary representing an
-        atom:Person construct.
-        """
-
-        if type(author) is dict:
-            return author
-        else:
-            if author.startswith("http://") or author.startswith("www"):
-                # This is clearly a URI
-                return {"uri" : author}
-            elif "@" in author and "." in author:
-                # This is most probably an email address
-                return {"email" : author}
-            else:
-                # Must be a name
-                return {"name" : author}
-
-    def _rssify_author(self, author):
-
-        """
-        Convert author from whatever it is to a plain old email string for
-        use in an RSS 2.0 feed.
-        """
-        
-        if type(author) is dict:
-            try:
-                return author["email"]
-            except KeyError:
-                return None
-        else:
-            if "@" in author and "." in author:
-                return author
-            else:
-                return None
-
-    def _add_subelems(self, root_element, mappings, dictionary):
-
-        """
-        Add one subelement to root_element for each key in dictionary
-        which is supported by a mapping in mappings
-        """
-
-        for mapping in mappings:
-            for key in mapping[0]:
-                if key in dictionary:
-                    if len(mapping) == 2:
-                        value = dictionary[key]
-                    elif len(mapping) == 3:
-                        value = mapping[2](dictionary[key])
-                    self._add_subelem(root_element, mapping[1], value)
-                    break                
-        
-    def _add_subelem(self, root_element, name, value):
-
-        if value is None:
-            return
-
-        if type(value) is dict:
-            ### HORRIBLE HACK!
-            if name=="link":
-                ET.SubElement(root_element, name, href=value["href"])
-            else:
-                subElem = ET.SubElement(root_element, name)
-                for key in value:
-                    self._add_subelem(subElem, key, value[key])
-        else:
-            ET.SubElement(root_element, name).text = value
-
-    def dump_to_file(self, tree, filename, pretty):
-
-        """
-        Dump an ElementTree to a file, with line breaks and indentation.
-        """
-
-        fp = open(filename, "w")
-        if pretty and feedformatterCanPrettyPrint:
-            doc = FromXml(ET.tostring(tree))
-            PrettyPrint(doc,fp,indent="    ")
-        else:
-            fp.write(ET.tostring(tree))
-        fp.close()
 
     ### RSS 1.0 STUFF ------------------------------
         
@@ -309,7 +309,7 @@ class Feed:
              "xmlns" : "http://purl.org/rss/1.0/"} )
         RSS1channel = ET.SubElement(RSS1root, 'channel',
             {"rdf:about" : self.feed["link"]})
-        self._add_subelems(RSS1channel, self.rss1_channel_mappings, self.feed)
+        _add_subelems(RSS1channel, _rss1_channel_mappings, self.feed)
         RSS1contents = ET.SubElement(RSS1channel, 'items')
         RSS1contents_seq = ET.SubElement (RSS1contents, 'rdf:Seq')
         for item in self.items:
@@ -317,8 +317,8 @@ class Feed:
         for item in self.items:
             RSS1item = ET.SubElement (RSS1root, 'item',
                 {"rdf:about" : item["link"]})
-            self._add_subelems(RSS1item, self.rss1_item_mappings, item)
-        self.dump_to_file(RSS1root, filename, pretty=False)
+            _add_subelems(RSS1item, _rss1_item_mappings, item)
+        _dump_to_file(RSS1root, filename, pretty=False)
 
     ### RSS 2.0 STUFF ------------------------------
 
@@ -356,10 +356,10 @@ class Feed:
             self.validate_rss2()
         RSS2root = ET.Element( 'rss', {'version':'2.0'} )
         RSS2channel = ET.SubElement( RSS2root, 'channel' )
-        self._add_subelems(RSS2channel, self.rss2_channel_mappings, self.feed)
+        _add_subelems(RSS2channel, _rss2_channel_mappings, self.feed)
         for item in self.items:            
             RSS2item = ET.SubElement ( RSS2channel, 'item' )
-            self._add_subelems(RSS2item, self.rss2_item_mappings, item)
+            _add_subelems(RSS2item, _rss2_item_mappings, item)
         self.dump_to_file(RSS2root, filename, pretty=pretty)
 
     def format_rss2_string(self, validate=True, pretty=False):
@@ -370,10 +370,10 @@ class Feed:
             self.validate_rss2()
         RSS2root = ET.Element( 'rss', {'version':'2.0'} )
         RSS2channel = ET.SubElement( RSS2root, 'channel' )
-        self._add_subelems(RSS2channel, self.rss2_channel_mappings, self.feed)
+        _add_subelems(RSS2channel, _rss2_channel_mappings, self.feed)
         for item in self.items:            
             RSS2item = ET.SubElement ( RSS2channel, 'item' )
-            self._add_subelems(RSS2item, self.rss2_item_mappings, item)
+            _add_subelems(RSS2item, _rss2_item_mappings, item)
         return ET.tostring(RSS2root)
 
     ### ATOM STUFF ------------------------------
@@ -403,11 +403,11 @@ class Feed:
         if validate:
             self.validate_atom()
         AtomRoot = ET.Element( 'feed', {"xmlns":"http://www.w3.org/2005/Atom"} )
-        self._add_subelems(AtomRoot, self.atom_feed_mappings, self.feed)
+        _add_subelems(AtomRoot, _atom_feed_mappings, self.feed)
         for entry in self.entries:
             AtomItem = ET.SubElement ( AtomRoot, 'entry' )
-            self._add_subelems(AtomItem, self.atom_item_mappings, entry)
-        self.dump_to_file(AtomRoot, filename, pretty=pretty)
+            _add_subelems(AtomItem, _atom_item_mappings, entry)
+        _dump_to_file(AtomRoot, filename, pretty=pretty)
 
 class InvalidFeedException(Exception):
 
@@ -425,7 +425,7 @@ def main():
 
     feed = Feed()
     feed.feed["title"] = "Test Feed"
-    feed.feed["link"] = "http://code.google.com/p/py-feedformatter/"
+    feed.feed["link"] = "http://code.google.com/p/feedformatter/"
     feed.feed["description"] = "A simple test feed for the feedformatter project"
     item = {}
     item["title"] = "Test item"
